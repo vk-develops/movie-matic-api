9c61462eca729590015f002badc3ff82574d4ebf
vk-develops <vimalkumarv291@gmail.com>
2023-08-15 14:47:39 +0530

    Added necessary packages and upadated

--- src/Components/MovieDetails/MovieDetailsCard.jsx
+++ src/Components/MovieDetails/MovieDetailsCard.jsx
@@ -4,14 +4,19 @@ import "./MovieDetailsCard.css";
 const MovieDetailsCard = ({ movieData }) => {
     return (
         <div className="card">
-            <h1>You have searched for: {movieData.Title} </h1>
             <div className="movie-card">
                 <div className="movie-header">
-                    <p>sdffvs</p>
+                    <div className="header-left">
+                        <h1> {movieData.Title} </h1>
+                        <h5> {movieData.Genre} </h5>
+                    </div>
+                    <div className="header-right">
+                        <h5> Ratings </h5>
+                    </div>
                 </div>
                 <div className="movie-contents">
                     <div className="movie-img">
-                        <img src={movieData.Poster} alt="" />
+                        <img src={movieData.Poster} alt="Poster" />
                     </div>
                     <div className="movie-texts"><p>sdv</p></div>
                 </div>
