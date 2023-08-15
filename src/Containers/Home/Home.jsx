e6cefa37b8f1f72d47df2bac598995e97c3ab191
vk-develops <vimalkumarv291@gmail.com>
2023-08-15 12:55:17 +0530

    Home section initial commit

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import "./Home.css";
+import searchIcon from "../../assets/Search.svg";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="home-section">
+            <div className="home-top">
+                <h1>MovieMatic.</h1>
+                <p>The MovieMatic is an movie app that displays all the contents regarding the movie you have searched for. Try this out by searching a movie in the below search bar</p>
+            </div>
+            <div className="search">
+                <input 
+                    type="text" 
+                    placeholder="Search for movies"
+                    value=""
+                    onChange={() => {}}
+                />
+                <img 
+                    src={searchIcon} 
+                    alt="Search" 
+                    onClick={() => {}}
+                />
+            </div>
+        </section>
     );
 }
 
