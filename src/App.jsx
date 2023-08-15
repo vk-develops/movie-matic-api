97fa936150121e46d4fae30c0df5bcd05727a13e
vk-develops <vimalkumarv291@gmail.com>
2023-08-15 12:36:12 +0530

    Added the navigation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import NavBar from "./Containers/NavBar/Navbar";
+import Home from "./Containers/Home/Home";
+import About from "./Containers/About/About";
+import Footer from "./Containers/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
-        <div>Hello World</div>
+        <React.Fragment>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+            <Footer />
+        </React.Fragment>
     );
 }
 
--- src/Containers/NavBar/Navbar.jsx
+++ src/Containers/NavBar/Navbar.jsx
@@ -3,7 +3,18 @@ import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="navigation">
+            <div className="nav-logo">
+                <a href="#"><h1>MovieMatic.</h1></a>
+            </div>
+            <div className="nav-elements">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <div className="nav-cta">
+                    <a href="#">Owner</a>
+                </div>
+            </div>
+        </nav>
     );
 }
 
